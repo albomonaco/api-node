88bd66578f37fc0c97b173a567391978675dec6f
Enzo Monaco <albomonaco@gmail.com>
2022-09-12 12:11:09 -0300

    First test with http request-response method

---
+++ server.js
@@ -0,0 +1,21 @@
+const http = require("http");
+const port = 3000;
+
+const rotas = {
+    '/': 'Hello World!',
+    '/livros': 'Which book do you want?',
+    '/autores': 'Listing authors',
+    'editora': 'editora'
+}
+
+const server = http.createServer((req,res) => {
+    res.writeHead(200, {'Content-type': 'text/plain'});
+    console.log(req.url);
+    res.end(rotas[req.url]);
+})
+
+
+server.listen(port, () => {
+    console.log(`Ouvindo em http://localhost:${port}`);
+})
+

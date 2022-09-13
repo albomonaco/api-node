8b87b3f21fef2e3d154363fca8548ae1c8c5b29d
Enzo Monaco <albomonaco@gmail.com>
2022-09-13 12:57:04 -0300

    Server updated with express

--- server.js
+++ server.js
@@ -1,20 +1,8 @@
-const http = require("http");
-const port = 3000;
+import app from './src/app.js'
 
-const rotas = {
-    '/': 'Hello World!',
-    '/livros': 'Which book do you want?',
-    '/autores': 'Listing authors',
-    '/editora': 'Listing publishers'
-}
+const port = process.env.PORT || 3000;
 
-const server = http.createServer((req,res) => {
-    res.writeHead(200, {'Content-type': 'text/plain'});
-    res.end(rotas[req.url]);
-})
-
-
-server.listen(port, () => {
+app.listen(port, () => {
     console.log(`Ouvindo em http://localhost:${port}`);
 })
 
